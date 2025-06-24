56e284f92367d9ef73e1225ba3fa639f00dbac22
kulikp1 <pashakylik2@gmail.com>
2025-06-24 23:52:11 +0300

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,49 +1,49 @@
-import { Routes, Route } from "react-router-dom";
-import "./App.css";
+// import { Routes, Route } from "react-router-dom";
+// import "./App.css";
 
-import HomePage from "../HomePage/HomePage";
-import SignupPage from "../SignUpPage/SignUp";
-import RecoveryTracker from "../RecoveryPage/RecoveryPage";
-import PrivateRoute from "../../PrivateRoute";
-import AboutPage from "../AboutPage/AboutPage";
-import AnalyticsPage from "../AnalyticsPage/AnalyticsPage";
-import Rewards from "../Rewards/Rewards";
-import AdminPage from "../AdminTasksPage/AdminTasksPage";
+// import HomePage from "../HomePage/HomePage";
+// import SignupPage from "../SignUpPage/SignUp";
+// import RecoveryTracker from "../RecoveryPage/RecoveryPage";
+// import PrivateRoute from "../../PrivateRoute";
+// import AboutPage from "../AboutPage/AboutPage";
+// import AnalyticsPage from "../AnalyticsPage/AnalyticsPage";
+// import Rewards from "../Rewards/Rewards";
+// import AdminPage from "../AdminTasksPage/AdminTasksPage";
 
-function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/signup" element={<SignupPage />} />
-      <Route path="/admin" element={<AdminPage />} />
+// function App() {
+//   return (
+//     <Routes>
+//       <Route path="/" element={<HomePage />} />
+//       <Route path="/signup" element={<SignupPage />} />
+//       <Route path="/admin" element={<AdminPage />} />
 
-      <Route
-        path="/recoveryTracker"
-        element={
-          <PrivateRoute>
-            <RecoveryTracker />
-          </PrivateRoute>
-        }
-      />
-      <Route path="/about" element={<AboutPage />} />
-      <Route
-        path="/stats"
-        element={
-          <PrivateRoute>
-            <AnalyticsPage />
-          </PrivateRoute>
-        }
-      />
-      <Route
-        path="/rewards"
-        element={
-          <PrivateRoute>
-            <Rewards />
-          </PrivateRoute>
-        }
-      />
-    </Routes>
-  );
-}
+//       <Route
+//         path="/recoveryTracker"
+//         element={
+//           <PrivateRoute>
+//             <RecoveryTracker />
+//           </PrivateRoute>
+//         }
+//       />
+//       <Route path="/about" element={<AboutPage />} />
+//       <Route
+//         path="/stats"
+//         element={
+//           <PrivateRoute>
+//             <AnalyticsPage />
+//           </PrivateRoute>
+//         }
+//       />
+//       <Route
+//         path="/rewards"
+//         element={
+//           <PrivateRoute>
+//             <Rewards />
+//           </PrivateRoute>
+//         }
+//       />
+//     </Routes>
+//   );
+// }
 
-export default App;
+// export default App;
